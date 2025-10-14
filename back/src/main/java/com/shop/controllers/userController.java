0d7c8e8b786d82f45d155c3d3570e090da6f14bc
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-14 15:35:22 +0200

    addition of controllers files

---
+++ back/src/main/java/com/shop/controllers/userController.java
@@ -0,0 +1,41 @@
+package com.shop.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shop.model.dto.userLogInDto;
+import com.shop.model.dto.userRegisterDto;
+import com.shop.model.response.simpleToken;
+import com.shop.services.userService;
+
+@RestController
+@RequestMapping("/auth")
+public class userController {
+    
+    @Autowired
+    public userService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> userRegister(@RequestBody userRegisterDto userRegisterDto){
+        String token = userService.register(userRegisterDto);
+
+        if(token.isEmpty())
+            return ResponseEntity.badRequest().body("Username already exist !!");
+
+        return ResponseEntity.ok().body(new simpleToken(token));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> userLogIn(@RequestBody userLogInDto userLogInDto){
+        String token = userService.logIn(userLogInDto);
+
+        if(token.isEmpty())
+            return ResponseEntity.badRequest().body("email or password is invalid !!");
+
+        return ResponseEntity.ok().body(new simpleToken(token));
+    }
+}
