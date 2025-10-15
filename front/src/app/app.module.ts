e1cf08fa7cb0f39f10ff57eb4a302c119adc48f4
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-15 12:15:23 +0200

    minor update for app.module

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
