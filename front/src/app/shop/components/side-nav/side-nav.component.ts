b074e9cc2addf9456a6cc3de6cdc7f77fcf6522b
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-16 14:48:53 +0200

    side nav component udpate

--- front/src/app/shop/components/side-nav/side-nav.component.ts
+++ front/src/app/shop/components/side-nav/side-nav.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrl: './side-nav.component.css'
+  styleUrl: './side-nav.component.scss'
 })
 export class SideNavComponent {
 
