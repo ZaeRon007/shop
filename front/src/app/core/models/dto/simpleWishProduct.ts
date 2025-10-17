7768eeb14f7c2f131d425d3fb3b2dfcd7c3c8d98
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-17 12:14:32 +0200

    functionnal update for wish component

---
+++ front/src/app/core/models/dto/simpleWishProduct.ts
@@ -0,0 +1,5 @@
+export class simpleWishProduct {
+    name!: string;
+    category!: string;
+    price!: number;
+}
--- front/src/app/shop/components/wish/wish.component.ts
+++ front/src/app/shop/components/wish/wish.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Subscription, BehaviorSubject, forkJoin, switchMap } from 'rxjs';
+import { productEntity } from '../../../core/models/productEntity';
+import { UserWishsService } from '../../services/userWishsService';
+import { userWishsEntity } from '../../../core/models/userWishsEntity';
 
 @Component({
   selector: 'app-wish',
@@ -6,5 +10,39 @@ import { Component } from '@angular/core';
   styleUrl: './wish.component.scss'
 })
 export class WishComponent {
+  wishsTab$ = new BehaviorSubject<productEntity[]>([]);
+  wishsLength = 0;
+  private sub: Subscription = new Subscription();
+  private sub1: Subscription = new Subscription();
 
+  constructor(private wishService: UserWishsService) {
+  }
+
+  ngOnInit(): void {
+    this.sub = this.wishService.getUserWishs().pipe(
+      switchMap((wishs: userWishsEntity[]) => {
+        this.wishsLength = wishs.length;
+        const requests = wishs.map(wish => this.wishService.getUserWish(wish.productId));
+        return forkJoin(requests);
+      })
+    ).subscribe({
+      next: (products) => {
+        this.wishsTab$.next(products);
+      },
+      error: (err) => console.error(err)
+    });
+
+  }
+
+  deleteFromWishs(id: number) {
+    this.wishService.removeFromWishs(id);
+    const updatedWishs = this.wishsTab$.getValue().filter(wish => wish.id != id);
+    this.wishsTab$.next(updatedWishs);
+    this.wishsLength = this.wishsTab$.getValue().length;
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+    this.sub1.unsubscribe();
+  }
 }
