6dc4e71213fc9c733d07ec9840c4660197544bca
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-17 12:14:15 +0200

    functional update for basker component

--- front/src/app/shop/components/basket/basket.component.ts
+++ front/src/app/shop/components/basket/basket.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UserBasketService } from '../../services/userBasketService';
-import { BehaviorSubject, combineLatest, map, Observable, Subscription, tap } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription, tap } from 'rxjs';
 import { productEntity } from '../../../core/models/productEntity';
-import { ProductService } from '../../services/productService';
 import { ProductWithQuantity } from '../../../core/models/ProductWithQuantity';
 import { GlobalService } from '../../services/globalService';
 import { userBasketEntity } from '../../../core/models/userBasketEntity';
@@ -13,11 +12,11 @@ import { userBasketEntity } from '../../../core/models/userBasketEntity';
   styleUrl: './basket.component.scss'
 })
 export class BasketComponent implements OnInit, OnDestroy {
-  private sub: Subscription = new Subscription();
   productsTab$ = new BehaviorSubject<productEntity[]>([]);
+  basketLength = 0;
+  private sub: Subscription = new Subscription();
   private sub1: Subscription = new Subscription();
   public productsWithQuantities$!: Observable<ProductWithQuantity[]>;
-  basketLength = 0;
 
   constructor(private basketService: UserBasketService,
     private globalService: GlobalService) {
@@ -25,9 +24,8 @@ export class BasketComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     // load user basket state
-    this.sub1 = this.basketService.getUserBasket().subscribe((res: userBasketEntity[]) => {
-      this.basketLength = res.length;
-      console.log("basket length :", this.basketLength);
+    this.sub1 = this.basketService.basket$.subscribe((basket: userBasketEntity[]) => {
+      this.basketLength = basket.length;
     });
 
     // combine latest updates and insert item quantity into productsWithQuantities
@@ -37,23 +35,15 @@ export class BasketComponent implements OnInit, OnDestroy {
 
   deleteFrombasket(id: number) {
     this.basketService.removeFromBasket(id);
-    this.updateBasketLength();
   }
 
   increase(productId: number, quantity: number) {
     this.basketService.increaseAmount(productId, quantity);
-    this.updateBasketLength();
   }
 
   decrease(productId: number, quantity: number) {
     this.basketService.decreaseAmount(productId, quantity);
-    this.updateBasketLength();
   }
-
-  updateBasketLength() {
-    this.basketLength = this.basketService.basket$.getValue.length;
-  }
-
 
   ngOnDestroy(): void {
     this.sub.unsubscribe();
