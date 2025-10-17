70a82f5a52b0420b86c06d69b6ff65d7c2d7cb12
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-17 10:20:13 +0200

    update for basket component

--- front/src/app/shop/components/basket/basket.component.ts
+++ front/src/app/shop/components/basket/basket.component.ts
@@ -5,6 +5,7 @@ import { productEntity } from '../../../core/models/productEntity';
 import { ProductService } from '../../services/productService';
 import { ProductWithQuantity } from '../../../core/models/ProductWithQuantity';
 import { GlobalService } from '../../services/globalService';
+import { userBasketEntity } from '../../../core/models/userBasketEntity';
 
 @Component({
   selector: 'app-basket',
@@ -16,6 +17,7 @@ export class BasketComponent implements OnInit, OnDestroy {
   productsTab$ = new BehaviorSubject<productEntity[]>([]);
   private sub1: Subscription = new Subscription();
   public productsWithQuantities$!: Observable<ProductWithQuantity[]>;
+  basketLength = 0;
 
   constructor(private basketService: UserBasketService,
     private globalService: GlobalService) {
@@ -23,7 +25,10 @@ export class BasketComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     // load user basket state
-    this.sub1 = this.basketService.getUserBasket().subscribe();
+    this.sub1 = this.basketService.getUserBasket().subscribe((res: userBasketEntity[]) => {
+      this.basketLength = res.length;
+      console.log("basket length :", this.basketLength);
+    });
 
     // combine latest updates and insert item quantity into productsWithQuantities
     this.productsWithQuantities$ = this.globalService.Init();
@@ -32,16 +37,23 @@ export class BasketComponent implements OnInit, OnDestroy {
 
   deleteFrombasket(id: number) {
     this.basketService.removeFromBasket(id);
-
+    this.updateBasketLength();
   }
 
   increase(productId: number, quantity: number) {
     this.basketService.increaseAmount(productId, quantity);
+    this.updateBasketLength();
   }
 
   decrease(productId: number, quantity: number) {
     this.basketService.decreaseAmount(productId, quantity);
+    this.updateBasketLength();
   }
+
+  updateBasketLength() {
+    this.basketLength = this.basketService.basket$.getValue.length;
+  }
+
 
   ngOnDestroy(): void {
     this.sub.unsubscribe();
