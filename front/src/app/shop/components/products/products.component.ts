01d8930e3e0acd5154dca8a784cd26f0d5fc576d
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-17 12:16:59 +0200

    clean up for product component

--- front/src/app/shop/components/products/products.component.ts
+++ front/src/app/shop/components/products/products.component.ts
@@ -15,16 +15,13 @@ import { userService } from '../../services/userService';
 export class ProductsComponent implements OnInit, OnDestroy {
   private sub1: Subscription = new Subscription();
   private sub2: Subscription = new Subscription();
-  private sub3: Subscription = new Subscription();
-  private sub4: Subscription = new Subscription();
   productsTab$ = new BehaviorSubject<productEntity[]>([]);
   public productsWithQuantities$!: Observable<ProductWithQuantity[]>;
   public isAdmin: boolean = false;
 
   constructor(private globalService: GlobalService,
     private basketService: UserBasketService,
-    private wishsService: UserWishsService,
-    private userService: userService) {
+    private wishsService: UserWishsService) {
   }
 
   ngOnInit(): void {
@@ -36,24 +33,10 @@ export class ProductsComponent implements OnInit, OnDestroy {
     this.sub2 = this.wishsService.getUserWishs().subscribe();
 
     this.productsWithQuantities$ = this.globalService.Init();
-
-    this.sub4 = this.userService.isAccountAdmin$().subscribe(res => {
-      if(res)
-        this.isAdmin = true;
-      else
-        this.isAdmin = false;
-    })
-  }
-
-  ngOnDestroy(): void {
-    this.sub1.unsubscribe();
-    this.sub2.unsubscribe();
-    this.sub3.unsubscribe();
-    this.sub4.unsubscribe();
   }
 
   isInWishList(product_id: number): boolean {
-    return this.wishsService.whishs$.value.some(wish => wish.productId === product_id);
+    return this.wishsService.wishsSubject.value.some(wish => wish.productId === product_id);
   }
 
   addToWishs(id: number) {
@@ -74,5 +57,10 @@ export class ProductsComponent implements OnInit, OnDestroy {
 
   decreaseAmount(id: number, quantity: number) {
     this.basketService.decreaseAmount(id, quantity);
+  }
+  
+  ngOnDestroy(): void {
+    this.sub1.unsubscribe();
+    this.sub2.unsubscribe();
   }
 }
