5e5acb6023a23032659481c66c11bf7949049c4a
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-19 13:39:01 +0200

    work in progress : setup for animation; partially working

--- front/src/app/shop/components/side-nav/side-nav.component.ts
+++ front/src/app/shop/components/side-nav/side-nav.component.ts
@@ -1,10 +1,22 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrl: './side-nav.component.scss'
+  styleUrls: ['./side-nav.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('closed', style({ transform: 'translateX(-120%)' })),
+      state('open', style({ transform: 'translateX(0)' })),
+      transition('closed <=> open', [animate('300ms ease-in-out')])
+    ])
+  ]
 })
 export class SideNavComponent {
+  protected menuState: 'open' | 'closed' = 'closed';
 
+  toggleMenu() {
+    this.menuState = this.menuState === 'closed' ? 'open' : 'closed';
+  }
 }
