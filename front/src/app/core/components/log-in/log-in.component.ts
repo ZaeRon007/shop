8da848dee5fc898357a3a864c905fb8d254438b0
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-21 15:37:28 +0200

    implementation of wobble animation in log-in component

--- front/src/app/core/components/log-in/log-in.component.ts
+++ front/src/app/core/components/log-in/log-in.component.ts
@@ -1,26 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { loginRequest } from '../../models/dto/loginRequest.interface';
 import { catchError, Subscription, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { transition, trigger, useAnimation, AnimationEvent } from '@angular/animations';
+import { wobbleAnimation } from '../../../../animations/wobble.animation';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrl: './log-in.component.scss'
+  styleUrl: './log-in.component.scss',
+  animations: [
+    trigger('wobble', [
+      transition('false => true', [
+        useAnimation(wobbleAnimation)
+      ])
+    ])
+  ]
 })
 export class LogInComponent {
   user: loginRequest = { email: "", password: "" };
   logInSubscription: Subscription = new Subscription();
   showError: boolean = false;
+  protected wobbleField = false;
 
 
   constructor(private router: Router,
-    private authService: AuthService) { }
+    private authService: AuthService,
+    private renderer: Renderer2) { }
 
 
   isFormValid(): boolean {
     return !!this.user.password.trim() && !!this.user.email.trim();
+  }
+
+  protected onWobbleStart(event: AnimationEvent) {
+    if (event.fromState !== 'void') {
+      this.renderer.addClass(event.element, 'invalid');
+    }
   }
 
 
@@ -30,6 +47,7 @@ export class LogInComponent {
         if (error.status == 400 || error.status == 401) {
           this.showError = true;
         }
+        this.wobbleField = true;
         return throwError(() => error);
       })
     ).subscribe((response: any) => {
@@ -37,6 +55,8 @@ export class LogInComponent {
       this.router.navigateByUrl(`/shop/home`);
     });
   }
+
+  
 
   ngOnDestroy(): void {
     this.logInSubscription.unsubscribe();
