4a1a4c9c10cc0e381b0556c18d7ffb2fb7fce9e5
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-22 17:56:30 +0200

    functional update for images in slider animation. Images are now responsive

--- front/src/app/shop/components/home/home.component.ts
+++ front/src/app/shop/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { transition, trigger, useAnimation } from '@angular/animations';
-import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit, signal } from '@angular/core';
 import { slideAnimation } from '../../../../animations/slide.animation';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 const animationDuration = 300;
 const animationInterval = 15000;
@@ -14,29 +15,54 @@ const animationInterval = 15000;
       transition('* => *', [
         useAnimation(slideAnimation)
       ], {
-        params: { 
-          leaveEnd: '100%', 
-          enterStart: '-100%', 
-          hiddenScale: 1, 
-          duration: animationDuration + 'ms' } 
+        params: {
+          leaveEnd: '100%',
+          enterStart: '-100%',
+          hiddenScale: 1,
+          duration: animationDuration + 'ms'
+        }
       })
     ])
   ]
 })
-export class HomeComponent implements OnInit, OnDestroy{
+export class HomeComponent implements OnInit, OnDestroy {
   public readonly appTitle = "SHOP";
+  screenSize$ = new BehaviorSubject<number>(window.innerWidth);
+
   images: string[] = [
-    "assets/background/city.jpeg",
-    "assets/background/city2.jpeg",
-    "assets/background/path.jpeg",
+    "assets/background/city-1920px.jpeg",
+    "assets/background/city_path-1920px.jpeg",
+    "assets/background/city_and_boat-1920px.jpeg",
   ];
 
   currentImageIndex = 0;
   animationState = signal(true);
   intervalId: any;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenSize$.next(event.target.innerWidth);
+  }
+
+  currentImage(inputSize: BehaviorSubject<number>): string {
+    const size = inputSize.value;
+    const picture = this.images[this.currentImageIndex];
+
+    if (size >= 950) {
+      return picture;
+    }
+    else if (size < 950 && size >= 450) {
+      const base = picture.replace("-1920px.jpeg", "");
+      return `${base}-800px.jpeg`;
+    }
+    else {
+      const base = picture.replace("-1920px.jpeg", "");
+        return `${base}-450px.jpeg`;
+    }
+  }
+  
   ngOnInit(): void {
-      this.startSlideshow()
+    this.startSlideshow()
   }
 
   startSlideshow() {
@@ -49,10 +75,6 @@ export class HomeComponent implements OnInit, OnDestroy{
     this.animationState.set(!this.animationState());
     this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
     setTimeout(() => this.animationState.set(!this.animationState()), animationDuration);
-  }
-
-  get currentImage() {
-    return this.images[this.currentImageIndex];
   }
 
   ngOnDestroy(): void {
