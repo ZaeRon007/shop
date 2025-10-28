76dc53ce9f8c4ad42d138db74ae31c649b01426e
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-28 17:34:33 +0100

    responsiveness update for contact component

--- front/src/app/shop/components/contact/contact.component.ts
+++ front/src/app/shop/components/contact/contact.component.ts
@@ -1,14 +1,29 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { pictureSizeService } from '../../services/pictureSizeService';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit, OnDestroy{
   form = { email: "", message: "" };
   private _snackbar = inject(MatSnackBar);
+  private picture: string[] = ['assets/background/contact-1920px.jpeg'];
+  private screenSizeSub = new Subscription();
+  currentImagePath = '';
+
+  constructor(private pictureService: pictureSizeService){
+    pictureService.setImagesTab(this.picture);
+  }
+
+  ngOnInit(): void {
+      this.screenSizeSub = this.pictureService.screenSize$.subscribe(() => {
+        this.currentImagePath = this.pictureService.currentStaticImage();
+      })
+  }
 
   public isFormValid() : boolean {
     return !!this.form.email.trim() && !!this.form.message.trim() && this.form.message.length < 300;
@@ -19,5 +34,9 @@ export class ContactComponent {
     this.form.message = "";
 
     this._snackbar.open("Demande de contact envoyée avec succès", "ok");
+  }
+
+  ngOnDestroy(): void {
+      this.screenSizeSub.unsubscribe();
   }
 }
