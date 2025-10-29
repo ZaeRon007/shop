a21f6974add8e31f51c312ecdf130ecd4fd02367
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-29 16:04:24 +0100

    401 gesture update for auth interceptor

--- front/src/app/core/interceptors/auth.interceptor.ts
+++ front/src/app/core/interceptors/auth.interceptor.ts
@@ -1,19 +1,27 @@
 import { HttpInterceptorFn } from "@angular/common/http";
 import { inject } from "@angular/core";
 import { AuthService } from "../services/auth.service";
+import { Router } from "@angular/router";
+import { catchError, throwError } from "rxjs";
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
-        const authService = inject(AuthService)
-        const authenticationToken = authService.getToken();
+    const authService = inject(AuthService);
+    const router = inject(Router);
+    const authenticationToken = authService.getToken();
 
-        if(authenticationToken){
-    
-            const modifiedReq = req.clone({
-                setHeaders: {
-                    Authorization: `Bearer ${authenticationToken}`,
-                },
-            });        
-            return next(modifiedReq);
-        }
-        return next(req);
+    const clonedReq = authenticationToken
+        ? req.clone({
+            setHeaders: { Authorization: `Bearer ${authenticationToken}` },
+        })
+        : req;
+
+    return next(clonedReq).pipe(
+        catchError((error) => {
+            if (error.status === 401) {
+                authService.logOut();
+                router.navigateByUrl('');
+            }
+            return throwError(() => error);
+        })
+    );
 }
