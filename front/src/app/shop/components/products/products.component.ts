00693602d2ad5ebe13dc2898f21b14094535238d
ZaeRon007 <julien.pelissier8@orange.fr>
2026-04-23 19:00:41 +0200

    clean up for products component

--- front/src/app/shop/components/products/products.component.ts
+++ front/src/app/shop/components/products/products.component.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { productEntity } from '../../../core/models/productEntity';
 import { UserBasketService } from '../../services/userBasketService';
 import { UserWishsService } from '../../services/userWishsService';
-import { ProductWithQuantity } from '../../../core/models/ProductWithQuantity';
+import { ProductWithQuantity } from '../../../core/models/dto/ProductWithQuantity';
 import { pictureSizeService } from '../../services/pictureSizeService';
 import { ProductService } from '../../services/productService';
 
@@ -13,8 +13,6 @@ import { ProductService } from '../../services/productService';
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent implements OnInit, OnDestroy {
-  private sub1: Subscription = new Subscription();
-  private sub2: Subscription = new Subscription();
   productsTab$ = new BehaviorSubject<productEntity[]>([]);
   public productsWithQuantities$!: Observable<ProductWithQuantity[]>;
   public isAdmin: boolean = false;
@@ -22,55 +20,20 @@ export class ProductsComponent implements OnInit, OnDestroy {
   private screenSizeSub = new Subscription();
   private picture: string[] = ['assets/background/commerce-1920px.jpeg'];
 
-  constructor(private basketService: UserBasketService,
-    private wishsService: UserWishsService,
-    private pictureService: pictureSizeService,
+  constructor(private pictureService: pictureSizeService,
     private productService: ProductService) {
       pictureService.setImagesTab(this.picture);
   }
 
   ngOnInit(): void {
-
-    // load user basket state
-    this.sub1 = this.basketService.getUserBasket().subscribe();
-
-    // load user wish list state
-    this.sub2 = this.wishsService.getUserWishs().subscribe();
-
     this.productsWithQuantities$ = this.productService.initProductList();
 
     this.screenSizeSub = this.pictureService.screenSize$.subscribe(() => {
       this.currentImagePath = this.pictureService.currentStaticImage();
     })
   }
-
-  isInWishList(product_id: number): boolean {
-    return this.wishsService.wishsSubject.value.some(wish => wish.productId === product_id);
-  }
-
-  addToWishs(id: number) {
-    this.wishsService.addToWishs(id);
-  }
-
-  removeFromWishs(id: number) {
-    this.wishsService.removeFromWishs(id);
-  }
-
-  addToCart(id: number, quantity: number) {
-    this.basketService.addToCart(id, quantity);
-  }
-
-  increaseAmount(id: number, quantity: number) {
-    this.basketService.increaseAmount(id, quantity);
-  }
-
-  decreaseAmount(id: number, quantity: number) {
-    this.basketService.decreaseAmount(id, quantity);
-  }
   
   ngOnDestroy(): void {
-    this.sub1.unsubscribe();
-    this.sub2.unsubscribe();
     this.screenSizeSub.unsubscribe();
   }
 }
